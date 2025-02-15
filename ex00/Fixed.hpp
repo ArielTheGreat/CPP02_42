b41d182b14aa882b0a0c69df35f4b2db07e109e8
Franco Rocha <franco.rocha@fast2work.de>
2025-02-15 18:43:55 +0100

    ex00 implemented

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -7,9 +7,16 @@
 class Fixed
 {
 	private:
-	
+		int					fix_point_value;
+		static const int	fractional_bits;
 	public:
-	
+		Fixed();
+		Fixed(const Fixed& object);
+		Fixed &operator=(const Fixed& object);
+		~Fixed();
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
 };
 
 #endif
